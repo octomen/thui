f3a109527d09ddfbe4ec1e19342364ffef8ab032
Kirill Medvedev <medvedev.kir93@gmail.com>
2019-05-13 00:30:21 +0500

    Добавил аватарку пользователя

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -18,13 +18,13 @@ export default {
   },
   methods: {
     setSessionToStoreId: async function setSessionToStoreId() {
-      const sessionId = cookies.get('session_id');
+      const sessionId = cookies.get('Authentication');
 
       if (!sessionId) {
         const { token } = queryString.parse(location.search);
         await this.$store.dispatch('auth/signIn', token);
 
-        return cookies.set('session_id', this.auth.sessionId, { expires: 1 });
+        return cookies.set('Authentication', this.auth.sessionId, { expires: 1 });
       }
 
       return this.$store.commit('auth/setSessionId', { sessionId });
--- client/src/store/auth.js
+++ client/src/store/auth.js
@@ -1,24 +1,40 @@
-import { getSessionIdByToken } from '../../api';
+import {
+  getSessionIdByToken,
+  getUserInfo
+} from '../../api';
 
 export const authState = {
   namespaced: true,
   state: {
     sessionId: '',
     user: {
-      login: ''
+      id: -1,
+      name: '',
+      avatar: ''
     }
   },
   mutations: {
     setSessionId(state, payload) {
       // eslint-disable-next-line no-param-reassign
       state.sessionId = payload.sessionId;
+    },
+    setUser(state, payload) {
+      // eslint-disable-next-line no-param-reassign
+      state.user = { ...state.user, ...payload };
     }
   },
   actions: {
-    async signIn({ commit }, token) {
+    async signIn({ commit, dispatch }, token) {
       const sessionId = await getSessionIdByToken(token);
 
       commit('setSessionId', { sessionId });
+
+      dispatch('setUserInfo', sessionId);
+    },
+    async setUserInfo({ commit }, sessionId) {
+      const user = await getUserInfo(sessionId);
+
+      commit('setUser', user);
     }
   }
 };
